fd35fe16025eeaed36df5607316b62fa4d67ae98
Rômulo Oliveira <romulolink@gmail.com>
2025-07-07 18:35:53 -0300

    [NEW] Integração com Tiled

---
+++ project/ground.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.11.2" name="ground" class="ground" tilewidth="16" tileheight="16" tilecount="240" columns="12" objectalignment="center">
+ <editorsettings>
+  <export target="../js/data/world.json" format="json"/>
+ </editorsettings>
+ <image source="../img/tileset/FarmRPG16x16-TinyAssetPack/Tileset/tileset.png" width="192" height="320"/>
+</tileset>
---
+++ project/trees.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="trees" class="trees" tilewidth="16" tileheight="16" tilecount="30" columns="10">
+ <grid orientation="orthogonal" width="48" height="16"/>
+ <image source="../img/tileset/FarmRPG16x16-TinyAssetPack/Objects/trees.png" width="160" height="48"/>
+</tileset>
