68e607cb000195e01839c41193227ed7dae0e6cb
Rômulo Oliveira <romulolink@gmail.com>
2025-07-07 21:50:32 -0300

    [NEW] Integração com o PhaserJS para tilemap

---
+++ project/props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.11.2" name="props" tilewidth="16" tileheight="16" tilecount="112" columns="14">
+ <image source="../img/tileset/FarmRPG16x16-TinyAssetPack/Objects/props.png" width="224" height="128"/>
+</tileset>
